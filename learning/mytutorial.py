2e031c9262a50ed8322bc2affb3ef23d3dadb9c6
Alexei Alayo <alexei.alayo@gmail.com>
2020-01-18 16:16:22 -0400

    some changes

--- learning/mytutorial.py
+++ learning/mytutorial.py
@@ -32,3 +32,24 @@ class TryAddingMoreText(Scene):
         author.match_color(quote2)
         self.play(FadeOut(quote))
 
+
+class ExerciseExampleIntegral(Scene):
+    def construct(self):
+        deriv, integral, v_t, dt, equals, v_T = formula = TexMobject(
+            "\\frac{d}{dT}", 
+            "\\int_0^T", "v(t)", "\\,dt", 
+            "=", "v(T)"
+        )
+
+        self.play(Write(formula))
+
+
+class ExerciseExample(Scene):
+    def construct(self):
+        eq1=TexMobject(r"\int_{C} (2 + x^2y) \,dx")
+        eq2=TexMobject(r"\int_{C} (2 + x^2y) dx = \int_{0}^{\pi} (2 + \cos^2 t \sin^2 t) \sqrt{\left(\frac{dx}{d t}\right)^2 + \left(\frac{dy}{dt}\right)^2} \,dt")
+        eq1.shift(2*UP)
+        #eq2.shift(1*UP)
+        self.play(Write(eq1))
+        self.play(Write(eq2))
+
