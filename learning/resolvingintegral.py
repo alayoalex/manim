b17108d7d35bda5f844db226f921e407c2444fd0
Alexei Alayo <alexei.alayo@gmail.com>
2020-02-28 02:22:11 -0400

    ...

--- learning/resolvingintegral.py
+++ learning/resolvingintegral.py
@@ -6,6 +6,34 @@ import numpy as np
 
 class ResonvingIntegral(Scene):
     def construct(self):
-        indefinite_integral_sign = "\\int"
-        definite_integral_sign = "\\int_a_b"
-        
+        circle = Circle()
+        square = Square()
+        square.flip(RIGHT)
+        square.rotate(-3 * TAU / 8)
+        circle.set_fill(PINK, opacity=0.2)
+
+        self.play(ShowCreation(square))
+        self.play(Transform(square, circle))
+        self.play(FadeOut(square))
+
+        self.wait()
+
+        example_text = TextMobject(
+            "Resuelve esta integral",
+            tex_to_color_map={"integral": YELLOW}
+        )
+        example_tex = TexMobject(
+            "\\int \\frac{1}{x^4 + 1} dx",
+        )
+        group = VGroup(example_text, example_tex)
+        group.arrange(DOWN)
+        group.set_width(FRAME_WIDTH - 2 * LARGE_BUFF)
+
+        self.play(Write(example_text))
+        self.play(Write(example_tex))
+        self.wait()
+
+        self.play(FadeOutAndShiftDown(example_text))
+        example_tex.scale(0.3)
+        example_tex.to_corner(UL)
+        self.play(Transform(example_tex, example_tex))
